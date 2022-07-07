a0e04e0db7d54e38ac290a1fc98f3db147803224
TH3VenGeuR <88973190+TH3VenGeuR@users.noreply.github.com>
2022-07-07 11:44:30 +0200

    convert loop to a function and call it 4 times to build packages

--- update-package-in-repo.sh
+++ update-package-in-repo.sh
@@ -1,54 +1,47 @@
 #!/bin/bash
 export today=$(date '+%Y%m%d')
 
-export VARPODMANVER=`curl https://api.github.com/repos/containers/podman/releases/latest | grep tag_name | awk '{print $2}' | tr -d '"' | tr -d ','`
-export VARCONMONVER=`curl https://api.github.com/repos/containers/conmon/releases/latest | grep tag_name | awk '{print $2}' | tr -d '"' | tr -d ','` 
-export VARPODMANDOWNLOAD="https://github.com/containers/podman.git"
-export VARCONMONDOWNLOAD="https://github.com/containers/conmon.git"
-export VARPODMANNAME=podman-rootless
-export VARCONMONNAME=conmon
-export VARPODMANHOMEPAGE="https://github.com/containers/podman/"
-export VARPODMANDEPS="slirp4netns crun"
-
-for i in rootless-containers/slirp4netns containers/crun; do
-    echo $i
-    export VARPKGARCH=x86_64
-    export VARMAINTAINER="adrien@vgr.pw"
-  if [[ $i =~ "slirp4netns" ]];then
-    export VARPKGVER=`curl https://api.github.com/repos/$i/releases/latest | grep tag_name | awk '{print $2}' | tr -d '"'  | tr -d ','`
-    export VARDOWNLOAD=`curl https://api.github.com/repos/$i/releases/latest | grep browser_download_url | grep -m1 x86_64 | awk '{print $2}'| tr -d '"'`
-    export VARPKGNAME=`echo $i | awk -F\/ '{print $2}'`
-    export VARPKGHOMEPAGE="https://github.com/rootless-containers/slirp4netns/"
-  elif [[ $i =~ "crun" ]];then
-    export VARPKGVER=`curl https://api.github.com/repos/$i/releases/latest | grep tag_name | awk '{print $2}'  | tr -d '"'  | tr -d ','`
-    export VARDOWNLOAD=`curl https://api.github.com/repos/$i/releases/latest | grep browser_download_url | grep -m1 amd64 | awk '{print $2}'  | tr -d '"' `
-    export VARPKGNAME=`echo $i | awk -F\/ '{print $2}'`
-    export VARPKGHOMEPAGE="https://github.com/containers/crun/"
-  fi
+create_packages (name, method, project_home, git_url, api_option, api_filter, depandancies, description) {
+  export VARMAINTAINER="adrien@vgr.pw"
+  export VARPKGARCH=x86_64
+  export VARPKGNAME="$name"
+  export VARPKGHOMEPAGE="$project_home"
+  export VARDEPS="$depandancies"
+  export VARPKGVER=`curl https://api.github.com/repos/$api_option/releases/latest | grep tag_name | awk '{print $2}' | tr -d '"'  | tr -d ','` 
+  export VARPKGDESCRIPTION="$description"
   envsubst < templates/manifest.tpl > /var/www/massos-repo/x86_64/manifest/$VARPKGNAME.manifest
-  mkdir -p /tmp/$VARPKGNAME-$today/usr/sbin/
-  curl $VARDOWNLOAD -o /tmp/$VARPKGNAME-$today/usr/sbin/$VARPKGNAME
-  cd /tmp/$VARPKGNAME-$today/
-  tar -cJf $VARPKGNAME-$VARPKGVER-$VARPKGARCH.tar.xz *
-  cp $VARPKGNAME-$VARPKGVER-$VARPKGARCH.tar.xz /var/www/massos-repo/x86_64/archives/
-  cd -
-done
+  mkdir -p /tmp/$VARPKGNAME-$today/usr/local
+  if [[ $method == git ]];then
+    if [[ $VARPKGNAME == "podman-rootless"]];then
+      export WORKDIR="podman"
+      export VARBUILDTAGS="BUILDTAGS='apparmor seccomp systemd'"
+    else;
+      export WORKDIR=$VARPKGNAME
+      export VARBUILDTAGS=""
+    export GOVERSION=$(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].version' | tr -d 'go')
+    cd /tmp/$VARPKGNAME-$today/
+    podman run --name gobuilder -d golang:$GOVERSION-bullseye sleep 3600
+    podman exec -it gobuilder apt update
+    podman exec -it gobuilder apt install git make libseccomp-dev libsystemd-dev libbtrfs-dev libdevmapper-dev libgpgme-dev libglib2.0-dev -y
+    podman exec -it gobuilder git clone $git_url
+    podman exec --workdir /go/$WORKDIR -it gobuilder git checkout $VARPKGVER
+    podman exec --workdir /go/$WORKDIR -it gobuilder make $VARBUILDTAGS
+    podman cp gobuilder:/go/$WORKDIR/bin/ usr/local/
+    cd /tmp/$VARPKGNAME-$today/
+    tar -cJf $VARPKGNAME-$VARPKGVER-$VARPKGARCH.tar.xz *
+    cp $VARPKGNAME-$VARPKGVER-$VARPKGARCH.tar.xz /var/www/massos-repo/x86_64/archives/
+    cd -
+  elif [[ $method == std ]];then
+    export VARDOWNLOAD=`curl https://api.github.com/repos/$api_option/releases/latest | grep browser_download_url | grep -m1 $filter | awk '{print $2}'| tr -d '"'`
+    curl $VARDOWNLOAD -o /tmp/$VARPKGNAME-$today/usr/local/$VARPKGNAME
+    cd /tmp/$VARPKGNAME-$today/
+    tar -cJf $VARPKGNAME-$VARPKGVER-$VARPKGARCH.tar.xz *
+    cp $VARPKGNAME-$VARPKGVER-$VARPKGARCH.tar.xz /var/www/massos-repo/x86_64/archives/  
+    cd -
+  fi
+}
 
-mkdir -p /tmp/$VARPODMANNAME-$today/usr/local/bin
-mkdir -p /tmp/$VARPODMANNAME-$today/usr/local/libexec
-mkdir -p /tmp/$VARCONMONNAME-$today/usr/local/libexec
-envsubst < templates/manifest.tpl > /var/www/massos-repo/x86_64/manifest/$VARPODMANNAME.manifest
-envsubst < templates/manifest.tpl > /var/www/massos-repo/x86_64/manifest/$VARCONMONNAME.manifest
-envsubst < templates/Dockerfile.tpl > Dockerfile
-podman build -t $VARPODMANNAME-$VARPODMANVER:$today .
-podman run -d $VARPODMANNAME-$VARPODMANVER:$today --name $VARPODMANNAME-$VARPODMANVER-$today
-podman cp $VARPODMANNAME-$VARPODMANVER-$today:/opt/podman/bin/rootlessport /tmp/$VARPODMANNAME-$today/usr/local/libexec/rootlessport
-podman cp $VARPODMANNAME-$VARPODMANVER-$today:/opt/conmon/bin/conmon /tmp/$VARCONMONNAME-$today/usr/local/libexec/conmon
-podman cp $VARPODMANNAME-$VARPODMANVER-$today:/opt/podman/bin/podman /tmp/$VARPODMANNAME-$today/usr/local/bin/podman
-
-cd /tmp/$VARCONMONNAME-$today/
-tar -cJf $VARCONMONNAME-$VARCONMONVER-$VARPKGARCH.tar.xz *
-cp $VARCONMONNAME-$VARCONMONVER-$VARPKGARCH.tar.xz /var/www/massos-repo/x86_64/archives/
-cd /tmp/$VARPODMANNAME-$today/
-tar -cJf $VARPODMANNAME-$VARPODMANVER-$VARPKGARCH.tar.xz *
-cp $VARPODMANNAME-$VARPODMANVER-$VARPKGARCH.tar.xz /var/www/massos-repo/x86_64/archives/
+create_packages(crun, std, "https://github.com/containers/crun/","", "containers/crun","amd64","","Crun is a container runtime written C" );
+create_packages(slirp4netns, std, "https://github.com/rootless-containers/slirp4netns/","", "rootless-containers/slirp4netns","x86_64","","Slirp4netns network layer for rootless container" );
+create_packages(podman-rootless, git, "https://podman.io", "https://github.com/containers/podman.git","containers/podman","","slirp4netns crun","Podman is container engine, istalled rootless" );
+create_packages(conmon, git, "https://github.com/containers/conmon", "https://github.com/containers/conmon.git","containers/conmon","","","Conmon is a monitoring program and communication tool between a container manager and an OCI runtime");
